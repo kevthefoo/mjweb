54f0247109eef4b7f5b7203838c481b9d6e782a9
KevTheFoo <kevthefoo@gmail.com>
2024-11-18 14:00:07 +0800

    Update landing page layout and add background image

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
-import { redirect } from "next/navigation";
-
-export default function page() {
-    redirect('/en/')
+export default async function Home() {
+    return (
+        <main>
+            <section className="min-h-screen flex flex-col gap-4 justify-center items-center bg-center bg-no-repeat	bg-cover text-center px-12 bg-[url('/inter.svg')]">
+                <h1 className="landing_page_title">
+                    Unlock Your Creativity with New Era Generative AI
+                </h1>
+                <a href="/home" className="landing_page_enter_button">
+                    Enter
+                </a>
+            </section>
+        </main>
+    );
 }
