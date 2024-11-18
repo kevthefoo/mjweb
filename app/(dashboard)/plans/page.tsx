5afd30cae11f96c1b0f10c78653b00991786f011
KevTheFoo <kevthefoo@gmail.com>
2024-11-19 01:58:27 +0800

    Remove Stripe pricing table script from Plans page and clean up section markup

--- app/(dashboard)/plans/page.tsx
+++ app/(dashboard)/plans/page.tsx
@@ -1,10 +1,5 @@
 export default async function Plans() {
     return (
-        <section className="h-full w-full border-4 border-white py-4">
-            <script
-                async
-                src="https://js.stripe.com/v3/pricing-table.js"
-            ></script>
-        </section>
+        <section className="h-full w-full border-4 border-white py-4"></section>
     );
 }
