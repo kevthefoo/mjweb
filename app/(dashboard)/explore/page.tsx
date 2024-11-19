464e65da107d1fdd9a1558242af672b394606875
KevTheFoo <kevthefoo@gmail.com>
2024-11-19 16:56:40 +0800

    Add image gallery to Explore page and update Tailwind configuration

--- app/(dashboard)/explore/page.tsx
+++ app/(dashboard)/explore/page.tsx
@@ -1,3 +1,100 @@
+import Image from "next/image";
+import image1 from "@/assets/1.png";
+import image2 from "@/assets/2.png";
+import image3 from "@/assets/3.png";
+import image4 from "@/assets/4.png";
+import image5 from "@/assets/5.png";
+import image6 from "@/assets/6.png";
+import image7 from "@/assets/7.png";
+import image8 from "@/assets/8.png";
+import image9 from "@/assets/9.png";
+
 export default async function Explore() {
-    return <section>THis is Explore page</section>;
+    return (
+        <section className="w-full h-full overflow-y-scroll">
+            <div className="columns-5 gap-0">
+                <div className="border-2 border-white">
+                    <Image src={image1} alt="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image2} alt="" className="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image3} alt="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image4} alt="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image5} alt="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image6} alt="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image7} alt="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image8} alt="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image9} alt="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image1} alt="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image2} alt="" className="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image3} alt="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image4} alt="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image5} alt="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image6} alt="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image7} alt="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image8} alt="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image9} alt="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image1} alt="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image2} alt="" className="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image3} alt="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image4} alt="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image5} alt="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image6} alt="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image7} alt="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image8} alt="" />
+                </div>
+                <div className="border-2 border-white">
+                    <Image src={image9} alt="" />
+                </div>
+            </div>
+        </section>
+    );
 }
