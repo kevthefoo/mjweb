10fb99413b08a53e2de70685aebd4379d7b1f8e7
KevTheFoo <kevthefoo@gmail.com>
2024-11-21 14:16:16 +0800

    Refactor Explore page to load images from remote sources; update image dimensions and styles for better display

--- app/(dashboard)/explore/page.tsx
+++ app/(dashboard)/explore/page.tsx
@@ -1,13 +1,4 @@
 import Image from "next/image";
-// import image1 from "@/assets/1.png";
-// import image2 from "@/assets/2.png";
-// import image3 from "@/assets/3.png";
-// import image4 from "@/assets/4.png";
-// import image5 from "@/assets/5.png";
-// import image6 from "@/assets/6.png";
-// import image7 from "@/assets/7.png";
-// import image8 from "@/assets/8.png";
-// import image9 from "@/assets/9.png";
 
 export default async function Explore() {
     return (
@@ -15,10 +6,98 @@ export default async function Explore() {
             <div className="columns-5 gap-0">
                 <div className="border-2 border-white">
                     <Image
-                        src="https://mjgallery.s3.us-east-1.amazonaws.com/0_0_384_N.webp"
+                        src="https://mjgallery.s3.us-east-1.amazonaws.com/4cdd4076-9f19-446a-af71-3b10d433bfb7.webp"
                         alt="ad"
-                        height={100}
-                        width={100}
+                        height={1000}
+                        width={1000}
+                        priority={true}
+                        style={{ height: "100%", width: "100%" }}
+                    />
+                </div>
+
+                <div className="border-2 border-white">
+                    <Image
+                        src="https://mjgallery.s3.us-east-1.amazonaws.com/a85cfed1-1473-4841-b1ee-1a1e954795b4.webp"
+                        alt="ad"
+                        height={1000}
+                        width={1000}
+                        priority={true}
+                        style={{ height: "100%", width: "100%" }}
+                    />
+                </div>
+
+                <div className="border-2 border-white">
+                    <Image
+                        src="https://mjgallery.s3.us-east-1.amazonaws.com/f4b24d3d-fe6b-44c5-b486-89196ff3d9ab.webp"
+                        alt="ad"
+                        height={1000}
+                        width={1000}
+                        priority={true}
+                        style={{ height: "100%", width: "100%" }}
+                    />
+                </div>
+
+                <div className="border-2 border-white">
+                    <Image
+                        src="https://mjgallery.s3.us-east-1.amazonaws.com/4b897c17-6a8c-4b9a-aef9-c022b0784c96.webp"
+                        alt="ad"
+                        height={1000}
+                        width={1000}
+                        priority={true}
+                        style={{ height: "100%", width: "100%" }}
+                    />
+                </div>
+
+                <div className="border-2 border-white">
+                    <Image
+                        src="https://mjgallery.s3.us-east-1.amazonaws.com/ea875246-9fe7-4c46-86f3-76123cd66284.webp"
+                        alt="ad"
+                        height={1000}
+                        width={1000}
+                        priority={true}
+                        style={{ height: "100%", width: "100%" }}
+                    />
+                </div>
+
+                <div className="border-2 border-white">
+                    <Image
+                        src="https://mjgallery.s3.us-east-1.amazonaws.com/6a4c9dd1-5976-46f3-98a4-b8aeafe9c74d.webp"
+                        alt="ad"
+                        height={1000}
+                        width={1000}
+                        priority={true}
+                        style={{ height: "100%", width: "100%" }}
+                    />
+                </div>
+
+                <div className="border-2 border-white">
+                    <Image
+                        src="https://mjgallery.s3.us-east-1.amazonaws.com/e65cf487-17f1-494a-9e31-cdc8a1e7ece0.webp"
+                        alt="ad"
+                        height={1000}
+                        width={1000}
+                        priority={true}
+                        style={{ height: "100%", width: "100%" }}
+                    />
+                </div>
+
+                <div className="border-2 border-white">
+                    <Image
+                        src="https://mjgallery.s3.us-east-1.amazonaws.com/ab2c7fc9-54aa-49d7-9316-40de46916422.webp"
+                        alt="ad"
+                        height={1000}
+                        width={1000}
+                        priority={true}
+                        style={{ height: "100%", width: "100%" }}
+                    />
+                </div>
+
+                <div className="border-2 border-white">
+                    <Image
+                        src="https://mjgallery.s3.us-east-1.amazonaws.com/77496a60-a19e-4f47-8d10-7aa2a8422249.webp"
+                        alt="ad"
+                        height={1000}
+                        width={1000}
                         priority={true}
                         style={{ height: "100%", width: "100%" }}
                     />
