7a62d1dd4bd32ca950afd98d4491c8b78ba38750
KevTheFoo <kevthefoo@gmail.com>
2024-12-04 19:22:43 +0800

    Enhance Account page responsiveness and adjust text sizes for better mobile display

--- app/(dashboard)/account/page.tsx
+++ app/(dashboard)/account/page.tsx
@@ -60,21 +60,21 @@ export default function Account() {
   }
 
   return (
-    <section className="flex h-full w-full flex-col overflow-y-scroll bg-neutral-800 px-20 pb-12 pt-16 text-center max-lg_mobile:pb-20">
+    <section className="flex h-full w-full flex-col overflow-y-scroll bg-neutral-800 px-20 pb-12 pt-16 text-center max-sm_tablet:px-10 max-lg_mobile:pb-20">
       <h1 className="text-3xl max-lg_mobile:text-xl">Manage Subscription</h1>
       <h3 className="mb-16 text-gray-500">
         Change or cancel your plan anytime
       </h3>
-      <div className="h-full rounded-xl border-[1px] bg-zinc-900 px-8 pt-4 max-lg_tablet:min-h-screen">
+      <div className="h-full w-full rounded-xl border-[1px] bg-zinc-900 px-8 pt-4 max-lg_tablet:min-h-screen max-sm_tablet:px-4">
         <div className="mb-8 flex items-center justify-between max-lg_tablet:flex-col max-lg_tablet:gap-4">
           <h1 className="text-3xl max-lg_tablet:text-2xl max-lg_mobile:text-xl">
             Your Plan
           </h1>
           <div className="flex gap-4">
-            <button className="rounded-full border-[1px] px-4 py-2 hover:bg-neutral-700 max-lg_tablet:text-sm max-lg_mobile:px-2 max-lg_mobile:py-1 max-lg_mobile:text-sm">
+            <button className="rounded-full border-[1px] px-4 py-2 hover:bg-neutral-700 max-lg_tablet:text-sm max-sm_tablet:px-2 max-sm_tablet:py-1 max-sm_tablet:text-xs">
               Change Plan
             </button>
-            <button className="rounded-full border-[1px] px-4 py-2 hover:bg-neutral-700 max-lg_tablet:text-sm max-lg_mobile:px-2 max-lg_mobile:py-1 max-lg_mobile:text-sm">
+            <button className="rounded-full border-[1px] px-4 py-2 hover:bg-neutral-700 max-lg_tablet:text-sm max-sm_tablet:px-2 max-sm_tablet:py-1 max-sm_tablet:text-xs">
               Cancel Plan
             </button>
           </div>
@@ -82,10 +82,10 @@ export default function Account() {
 
         <div className="flex items-center justify-center gap-20 max-lg_tablet:flex-col">
           <div className="w-1/2 rounded-xl border-[1px] bg-gray-900 px-4 py-4 text-start max-lg_tablet:w-full">
-            <h1 className="mb-4 text-2xl max-lg_tablet:text-xl max-lg_mobile:text-xl">
+            <h1 className="mb-4 text-2xl max-lg_tablet:text-xl max-sm_tablet:text-lg max-lg_mobile:text-base">
               {userSubData?.planName}
             </h1>
-            <ul className="flex flex-col gap-4 max-lg_tablet:text-base max-lg_mobile:text-sm">
+            <ul className="flex flex-col gap-4 max-lg_tablet:text-base max-sm_tablet:text-xs">
               {userSubData?.features.map((item, keys) => {
                 return (
                   <li key={keys}>
@@ -96,10 +96,10 @@ export default function Account() {
             </ul>
           </div>
           <div className="w-1/2 rounded-xl border-[1px] bg-gray-900 px-4 py-4 text-start max-lg_tablet:w-full">
-            <h1 className="mb-4 text-2xl max-lg_tablet:text-xl max-lg_mobile:text-lg">
+            <h1 className="mb-4 text-2xl max-lg_tablet:text-xl max-sm_tablet:text-lg max-lg_mobile:text-base">
               Billing & Payment
             </h1>
-            <ul className="flex flex-col gap-4 max-lg_tablet:text-base max-lg_mobile:text-sm">
+            <ul className="flex flex-col gap-4 max-lg_tablet:text-base max-sm_tablet:text-xs">
               <li>
                 <p>Price: {userSubData?.price}</p>
               </li>
